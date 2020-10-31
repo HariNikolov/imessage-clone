b1dd48552a47ee625146589fdb1b7f847b3e7037
HariNikolov <hakkonikoloff@gmail.com>
2020-10-31 15:17:34 +0200

    create the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Imessage from "./Imessage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build the iMessage Clone</h1>
+      <Imessage />
     </div>
   );
 }
