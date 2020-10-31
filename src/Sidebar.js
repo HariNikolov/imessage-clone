38244cfdbb5e6413a54532639e344433e936f244
HariNikolov <hakkonikoloff@gmail.com>
2020-10-31 22:06:09 +0200

    fix the chat groups

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,29 +1,59 @@
 import { Avatar, IconButton } from "@material-ui/core";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import SearchIcon from "@material-ui/icons/Search";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
+import db, { auth } from "./firebase";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const addChat = () => {
+    const chatName = prompt("Please enter a chat name");
+    if (chatName) {
+      db.collection("chats").add({
+        chatName,
+      });
+    }
+  };
+
   return (
     <div className="sidebar">
-      <div className="sidebar__header">
-        <Avatar className="sidebar__avatar" />
+    <div className="sidebar__header">
+        <Avatar
+          onClick={() => auth.signOut()}
+          src={user.photo}
+          className="sidebar__avatar"
+        />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
         </div>
 
         <IconButton variant="outlined" className="sidebar__inputButton">
-          <RateReviewOutlinedIcon />
+          <RateReviewOutlinedIcon onClick={addChat} />
         </IconButton>
       </div>
 
       <div className="sidebar__chats">
-          <SidebarChat />
-          <SidebarChat />
-          <SidebarChat />
+        {chats.map(({ id, data: { chatName } }) => (
+          <SidebarChat key={id} id={id} chatName={chatName} />
+        ))}
       </div>
     </div>
   );
