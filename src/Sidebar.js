5bd573cd545593ed131c1b0ecf9399e43a3930e4
HariNikolov <hakkonikoloff@gmail.com>
2020-10-31 17:43:09 +0200

    create sidebarChat component and css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,24 +3,25 @@ import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar className="sidebar__avatar"/>
-
+        <Avatar className="sidebar__avatar" />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
         </div>
+
         <IconButton variant="outlined" className="sidebar__inputButton">
           <RateReviewOutlinedIcon />
         </IconButton>
       </div>
 
       <div className="sidebar__chats">
-
+          <SidebarChat />
       </div>
     </div>
   );
